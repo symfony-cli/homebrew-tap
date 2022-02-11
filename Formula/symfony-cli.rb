b81307fb82b4e62be4e7602b748abc426e3ebc3a
Fabien Potencier <fabien@symfony.com>
2022-02-11 16:13:10 +0100

    Brew formula update for symfony-cli version v5.3.4

--- Formula/symfony-cli.rb
+++ Formula/symfony-cli.rb
@@ -5,12 +5,12 @@
 class SymfonyCli < Formula
   desc "Symfony CLI helps Symfony developers manage projects, from local code to remote infrastructure"
   homepage "https://symfony.com"
-  version "5.3.3"
+  version "5.3.4"
   license "AGPL-3.0"
 
   on_macos do
-    url "https://github.com/symfony-cli/symfony-cli/releases/download/v5.3.3/symfony-cli_darwin_all.tar.gz"
-    sha256 "d9d63e702701760aa847b9ad5273d22a33f70430f72ea74844ca3b33c4943d81"
+    url "https://github.com/symfony-cli/symfony-cli/releases/download/v5.3.4/symfony-cli_darwin_all.tar.gz"
+    sha256 "8a15bcbeb700f5daa38428479c3a1a6abe9ffe5e13e1ab1e42b0c384cde11966"
 
     def install
       bin.install "symfony"
@@ -18,17 +18,17 @@ class SymfonyCli < Formula
   end
 
   on_linux do
-    if Hardware::CPU.intel?
-      url "https://github.com/symfony-cli/symfony-cli/releases/download/v5.3.3/symfony-cli_linux_amd64.tar.gz"
-      sha256 "44d87619b70ba533643bb354f4897267bb274c9adbeab5c8993b28cbd48a7c97"
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/symfony-cli/symfony-cli/releases/download/v5.3.4/symfony-cli_linux_arm64.tar.gz"
+      sha256 "ef7d91dc4d1db7c30bd032ad430195c581c46dc2156c21b6cb4feba2a7c60a27"
 
       def install
         bin.install "symfony"
       end
     end
-    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
-      url "https://github.com/symfony-cli/symfony-cli/releases/download/v5.3.3/symfony-cli_linux_arm64.tar.gz"
-      sha256 "7046a70efa5843a47f40d3ba27c1fbf7f686d611ca2e87a60006699ef5f17721"
+    if Hardware::CPU.intel?
+      url "https://github.com/symfony-cli/symfony-cli/releases/download/v5.3.4/symfony-cli_linux_amd64.tar.gz"
+      sha256 "0195dea95df5545613f7b23a8265dbb3330b1687ad1d4eb1695f8f468d3d36be"
 
       def install
         bin.install "symfony"
